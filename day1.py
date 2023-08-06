d3aa34530ada7f8e9091f219f67b92ed9204c004
Prabin <42871240+prabincankod@users.noreply.github.com>
2023-08-06 15:57:24 +0000

    :sparkles: day one completed

---
+++ day1.py
@@ -0,0 +1,9 @@
+num1 = int(input("enter a number"))
+num2 = int(input("enter another number"))
+
+sum = num1+num2
+diff=num1-num2
+product= num1*num2
+division = num2 //num1
+
+print(f"The sum is :{sum}. \n The difference is :{diff} \n The product is :{product}. \n The Division is :{division}")
